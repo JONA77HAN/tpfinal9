41a1775d5cd007a4a984c9e5fa1a33b41eb0068a
Jonathan <jonathancarranza70@gmail.com>
2023-04-16 16:30:42 -0300

    primer commit de los ultimos

---
+++ BACKEND/src/test/java/com/Portfolio/Jonathan/JonathanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.Jonathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonathanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
